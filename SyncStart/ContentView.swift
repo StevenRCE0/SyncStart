553ce151cf441d13e673cd27b1bb728c0030f4d4
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2024-01-30 19:28:39 +0800

    Initial Commit

---
+++ SyncStart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SyncStart
+//
+//  Created by 砚渤 on 2024/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncStart/SyncStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncStartApp.swift
+//  SyncStart
+//
+//  Created by 砚渤 on 2024/1/30.
+//
+
+import SwiftUI
+
+@main
+struct SyncStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
